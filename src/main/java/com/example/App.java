589342ef0d4049e9cc72ede96b58ac65070e649a
ronallima <ronald_lima@outlook.com>
2022-10-27 20:19:34 -0300

    atualizacao login

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -7,20 +7,47 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
 
+    private double x;
+    private double y;
+    
     @Override
     public void start(Stage palco) throws IOException {
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("view/principal.fxml"));
+        palco.initStyle(StageStyle.TRANSPARENT);
         Parent root = loader.load();
+
+
+        root.setOnMousePressed((MouseEvent e)->{ 
+            x = e.getSceneX();
+            y = e.getSceneY();
+            
+        });
+        
+        root.setOnMouseDragged((MouseEvent e)->{
+        
+            palco.setX(e.getScreenX()-x);
+            palco.setY(e.getScreenY()-y);
+            palco.setOpacity(0.4);
+        });
+        
+        root.setOnMouseReleased((MouseEvent e )->{
+            palco.setOpacity(1);
+        });
+
+
         palco.setScene(new Scene(root));
         palco.show();
+    
     }
 
     public static void main(String[] args) {
